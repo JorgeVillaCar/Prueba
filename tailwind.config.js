58fa53a15a46c3def30d82457f16d716674cc543
JorgeVillaCar <112780556+JorgeVillaCar@users.noreply.github.com>
2022-09-03 14:47:52 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'resources/css/app.css',
+    'resources/js/app.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
